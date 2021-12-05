e327fd4fed7bc4e1237a6671e31e976933fb21dd
golimani <95503028+golimani@users.noreply.github.com>
2021-12-05 14:02:39 -0600

    Update SimpleForm.py

--- Chapter-1/SimpleForm.py
+++ Chapter-1/SimpleForm.py
@@ -1,31 +1,13 @@
-from flask import Flask, render_template, flash, request
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
-
-# App config.
-DEBUG = True
+from flask import Flask, render_template,request
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
-class ReusableForm(Form):
-    name = TextField('Name:', validators=[validators.required()])
-    
-    @app.route("/", methods=['GET', 'POST'])
-    def hello():
-        form = ReusableForm(request.form)
-    
-        print form.errors
-        if request.method == 'POST':
-            name=request.form['name']
-            print name
-    
-        if form.validate():
-            # Save the comment here.
-            flash('Hello ' + name)
-        else:
-            flash('All the form fields are required. ')
-    
-    return render_template('hello.html', form=form)
+@app.route('/',methods = ['GET'])
+@app.route('/send',methods = ['GET','POST'])
+def send():
+    if request.method == 'POST':
+        age = request.form['age']
+        return render_template('age.html',age=age)
+    return render_template('index.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
